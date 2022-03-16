5ce64f227167263767add2f3f11349d25cc54fb8
Luiz Carlos Bessa de Lima <luizcarlosbessa9@gmail.com>
2022-03-16 10:16:27 -0300

    Aula: Funçao Map #01

---
+++ funcao/map1.js
@@ -0,0 +1,24 @@
+const nums = [1, 2, 3, 4, 5];
+
+//For com propósito
+
+let resultado = nums.map(function (e) {
+  return e * 2;
+});
+
+console.log(resultado);
+
+console.log(
+  "O Array nums tem os seguintes números:" +
+    nums +
+    " e a sua transformação para o dobro é o Array resultado:" +
+    resultado
+);
+
+const soma10 = (e) => e + 10;
+const triplo = (e) => e * 3;
+const paraDinheiro = (e) => `R$ ${parseFloat(e).toFixed(2).replace(".", ",")}`;
+
+resultado = nums.map(soma10).map(triplo).map(paraDinheiro);
+
+console.log(resultado);
